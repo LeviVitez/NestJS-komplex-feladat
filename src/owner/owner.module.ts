a232aeb782c2af140566e7ecb5bd216b5dc0d910
LeviVitez <bmwyolo555@gmail.com>
2023-03-29 13:46:26 +0200

    haladás

--- src/owner/owner.module.ts
+++ src/owner/owner.module.ts
@@ -2,8 +2,12 @@
 import { Module } from '@nestjs/common';
 import { OwnerService } from './owner.service';
 import { OwnerController } from './owner.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Owner } from './entities/owner.entity';
+import { AccountModule } from 'src/account/account.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Owner])],
   controllers: [OwnerController],
   providers: [OwnerService]
 })
